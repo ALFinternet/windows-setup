2e5827ef371328b5fdeb7b636c4c3fdcc65627ed
ALFinternet <ALFinternet@gmail.com>
2023-06-23 16:04:05 -0700

    more items

--- Admin Desktop Setup.ps1
+++ Admin Desktop Setup.ps1
@@ -1,4 +1,6 @@
 # Setup my admin desktop computer
+Install-PackageProvider -Name NuGet -Force
+Install-Module -Name PowerShellGet -Force -AllowClobber
 
 # Install RSAT Tools
 Add-WindowsCapability -Online -Name "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0"
@@ -7,3 +9,10 @@ Add-WindowsCapability -Online -Name "Rsat.DHCP.Tools~~~~0.0.1.0"
 Add-WindowsCapability -Online -Name "Rsat.Dns.Tools~~~~0.0.1.0"
 Add-WindowsCapability -Online -Name "Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0"
 Add-WindowsCapability -Online -Name "Rsat.ServerManager.Tools~~~~0.0.1.0"
+
+# Install M365 Powershells
+Install-Module -Name MSOnline -Force
+Install-Module -Name AzureAD -Force
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Force
+Install-Module -Name MicrosoftTeams -Force -AllowClobber
+Install-Module -Name ExchangeOnlineManagement -Force
