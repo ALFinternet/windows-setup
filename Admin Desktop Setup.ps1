0447c3268e12f5eb2dc7095ac14ecdcdfd4c5612
ALFinternet <ALFinternet@gmail.com>
2023-07-08 15:51:42 -0700

    update to ps1

--- Admin Desktop Setup.ps1
+++ Admin Desktop Setup.ps1
@@ -10,6 +10,7 @@ Add-WindowsCapability -Online -Name "Rsat.Dns.Tools~~~~0.0.1.0"
 Add-WindowsCapability -Online -Name "Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0"
 Add-WindowsCapability -Online -Name "Rsat.ServerManager.Tools~~~~0.0.1.0"
 
+
 # Install M365 Powershells
 Install-Module -Name MSOnline -Force
 Install-Module -Name AzureAD -Force
