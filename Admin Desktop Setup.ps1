2f6dcae47ad879e47763f6250b801b4553a700f6
ALFinternet <ALFinternet@gmail.com>
2023-10-22 17:32:07 -0700

    update winrm

--- Admin Desktop Setup.ps1
+++ Admin Desktop Setup.ps1
@@ -11,7 +11,8 @@ Add-WindowsCapability -Online -Name "Rsat.Dns.Tools~~~~0.0.1.0"
 Add-WindowsCapability -Online -Name "Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0"
 
 # Enable WinRM - https://learn.microsoft.com/en-us/powershell/exchange/exchange-online-powershell-v2?view=exchange-ps#turn-on-basic-authentication-in-winrm
-winrm set winrm/config/client/auth '@{Basic="true"}'
+#winrm set winrm/config/client/auth '@{Basic="true"}'
+winrm quickconfig -force
 
 # Install M365 Powershells
 Install-Module -Name MSOnline -Force
