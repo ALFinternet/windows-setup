5f81a26d0266cca9902f1594de8974518ef56892
ALFinternet <ALFinternet@gmail.com>
2023-10-22 17:29:41 -0700

    Add winrm

--- Admin Desktop Setup.ps1
+++ Admin Desktop Setup.ps1
@@ -3,13 +3,15 @@ Install-PackageProvider -Name NuGet -Force
 Install-Module -Name PowerShellGet -Force -AllowClobber
 
 # Install RSAT Tools
+Add-WindowsCapability -Online -Name "Rsat.ServerManager.Tools~~~~0.0.1.0"
 Add-WindowsCapability -Online -Name "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0"
 Add-WindowsCapability -Online -Name "Rsat.CertificateServices.Tools~~~~0.0.1.0"
 Add-WindowsCapability -Online -Name "Rsat.DHCP.Tools~~~~0.0.1.0"
 Add-WindowsCapability -Online -Name "Rsat.Dns.Tools~~~~0.0.1.0"
 Add-WindowsCapability -Online -Name "Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0"
-Add-WindowsCapability -Online -Name "Rsat.ServerManager.Tools~~~~0.0.1.0"
 
+# Enable WinRM - https://learn.microsoft.com/en-us/powershell/exchange/exchange-online-powershell-v2?view=exchange-ps#turn-on-basic-authentication-in-winrm
+winrm set winrm/config/client/auth '@{Basic="true"}'
 
 # Install M365 Powershells
 Install-Module -Name MSOnline -Force
