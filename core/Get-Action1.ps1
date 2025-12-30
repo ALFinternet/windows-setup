38f3bebc0cda272771b87e30f970b29ec265d16d
alfinternet <alfinternet@gmail.com>
2025-12-29 23:02:23 -0800

    init

---
+++ core/Get-Action1.ps1
@@ -0,0 +1,3 @@
+# Installs Action1 Agent
+# 2025-12-29 AF
+curl -o "action1_agent(My_Organization).msi" "https://app.action1.com/agent/b8107334-6caa-11ee-b370-afdbb3c92bac/Windows/agent(My_Organization).msi" && msiexec /i "action1_agent(My_Organization).msi" /quiet /qn
